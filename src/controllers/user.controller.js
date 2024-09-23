9291e84926cca2164378ee1392e351e204317260
Akash Agrawal <akashagrawalecs@gmail.com>
2024-09-23 14:30:46 +0530

    all setup configured

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,11 @@
+import asyncHandler from "../utils/asynchHandler.js";
+
+const registerUser= asyncHandler(async(req,res)=>{
+    res.status(200).json({
+        message:"Chai aur Code"
+    })
+})
+
+
+export {registerUser}
+
